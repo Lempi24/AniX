9c839bc141270b24a1d16140340e59cddf4b3113
Lempi24 <ShadowHunter69@wp.pl>
2026-07-14 23:08:21 +0200

    baner changes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,60 +1,70 @@
-import Home from './Pages/Home';
-import { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navigation from './Components/Navigation';
-import AnimeDetails from './Pages/AnimeDetails';
-import PlayerPage from './Pages/PlayerPage';
-import Anime from './Pages/Anime';
-import ScrollToTop from './Components/ScrollToTop';
-import LoginPopup from './Components/LogInPopup';
-import { Toaster } from 'react-hot-toast';
-import Footer from './Pages/Footer';
-import Dmca from './Pages/Dmca';
-import PrivacyPolicy from './Pages/PrivacyPolicy';
-import TermsOfService from './Pages/TermsOfService';
-import ContactPage from './Pages/ContactPage';
-import MyList from './Pages/MyList';
+import Home from "./Pages/Home";
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navigation from "./Components/Navigation";
+import AnimeDetails from "./Pages/AnimeDetails";
+import PlayerPage from "./Pages/PlayerPage";
+import Anime from "./Pages/Anime";
+import ScrollToTop from "./Components/ScrollToTop";
+import LoginPopup from "./Components/LogInPopup";
+import { Toaster } from "react-hot-toast";
+import Footer from "./Pages/Footer";
+import Dmca from "./Pages/Dmca";
+import PrivacyPolicy from "./Pages/PrivacyPolicy";
+import TermsOfService from "./Pages/TermsOfService";
+import ContactPage from "./Pages/ContactPage";
+import MyList from "./Pages/MyList";
 function App() {
-	const [isLoginOpen, setIsLoginOpen] = useState(false);
-	return (
-		<BrowserRouter>
-			<ScrollToTop />
-			<Navigation onLoginClick={() => setIsLoginOpen(true)} />
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route
-					path='/anime/:animeId'
-					element={<AnimeDetails loginPopup={() => setIsLoginOpen(true)} />}
-				/>
-				<Route path='/watch/:animeId/:episodeNumber' element={<PlayerPage />} />
-				<Route path='/anime' element={<Anime />} />
-				<Route path='/dmca' element={<Dmca />} />
-				<Route path='/privacy-policy' element={<PrivacyPolicy />} />
-				<Route path='/terms-of-service' element={<TermsOfService />} />
-				<Route path='/contact' element={<ContactPage />} />
-				<Route path='/my-list' element={<MyList />} />
-			</Routes>
-			<Toaster
-				position='top-center'
-				reverseOrder={false}
-				toastOptions={{
-					duration: 2000,
-					style: {
-						background: '#222831',
-						color: '#e3e2e2',
-						fontSize: '1.3rem',
-						maxWidth: 'fit-content',
-						width: 'fit-content',
-						minWidth: 'unset',
-					},
-					success: {},
-					error: {},
-				}}
-			/>
-			<Footer />
-			<LoginPopup isOpen={isLoginOpen} onClose={() => setIsLoginOpen(false)} />
-		</BrowserRouter>
-	);
+  const [isLoginOpen, setIsLoginOpen] = useState(false);
+  return (
+    <BrowserRouter>
+      <div className="min-h-screen flex flex-col">
+        <ScrollToTop />
+        <Navigation onLoginClick={() => setIsLoginOpen(true)} />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/anime/:animeId"
+              element={<AnimeDetails loginPopup={() => setIsLoginOpen(true)} />}
+            />
+            <Route
+              path="/watch/:animeId/:episodeNumber"
+              element={<PlayerPage />}
+            />
+            <Route path="/anime" element={<Anime />} />
+            <Route path="/dmca" element={<Dmca />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="/terms-of-service" element={<TermsOfService />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/my-list" element={<MyList />} />
+          </Routes>
+        </main>
+        <Toaster
+          position="top-center"
+          reverseOrder={false}
+          toastOptions={{
+            duration: 2000,
+            style: {
+              background: "#222831",
+              color: "#e3e2e2",
+              fontSize: "1.3rem",
+              maxWidth: "fit-content",
+              width: "fit-content",
+              minWidth: "unset",
+            },
+            success: {},
+            error: {},
+          }}
+        />
+        <Footer />
+        <LoginPopup
+          isOpen={isLoginOpen}
+          onClose={() => setIsLoginOpen(false)}
+        />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -1,164 +1,163 @@
-import { ImageCarousel } from '../Components/ImageCarousel';
-import AnimeCardSection from '../Components/AnimeCardSection';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import AnimeCardSkeleton from '../Components/Anime/AnimeCardSkeleton';
+import { ImageCarousel } from "../Components/ImageCarousel";
+import AnimeCardSection from "../Components/AnimeCardSection";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import AnimeCardSkeleton from "../Components/Anime/AnimeCardSkeleton";
 const Home = () => {
-	const navigate = useNavigate();
-	const [popularAnime, setPopularAnime] = useState([]);
-	const [recentAnime, setRecentAnime] = useState([]);
-	const [recomendedAnime, setRecommendedAnime] = useState([]);
-	const [loading, setLoading] = useState(true);
-	const fetchAllData = async () => {
-		setLoading(true);
-		try {
-			const requests = [
-				axios.get(`${import.meta.env.VITE_BACKEND_URL}/anime/popular`),
-				axios.get(`${import.meta.env.VITE_BACKEND_URL}/anime/recent`),
-				axios.get(`${import.meta.env.VITE_BACKEND_URL}/anime/recommended`),
-			];
+  const navigate = useNavigate();
+  const [popularAnime, setPopularAnime] = useState([]);
+  const [recentAnime, setRecentAnime] = useState([]);
+  const [recomendedAnime, setRecommendedAnime] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const fetchAllData = async () => {
+    setLoading(true);
+    try {
+      const requests = [
+        axios.get(`${import.meta.env.VITE_BACKEND_URL}/anime/popular`),
+        axios.get(`${import.meta.env.VITE_BACKEND_URL}/anime/recent`),
+        axios.get(`${import.meta.env.VITE_BACKEND_URL}/anime/recommended`),
+      ];
 
-			const [popularResponse, recentResponse, recomendedResponse] =
-				await Promise.all(requests);
-			setRecommendedAnime(recomendedResponse.data);
-			setPopularAnime(popularResponse.data);
-			setRecentAnime(recentResponse.data);
-		} catch (error) {
-			console.error('Error fetching popular anime:', error);
-		} finally {
-			setLoading(false);
-		}
-	};
-	useEffect(() => {
-		fetchAllData();
-	}, []);
-	const handleAnimeClick = (animeObject) => {
-		if (animeObject && animeObject.mal_id) {
-			navigate(`/anime/${animeObject.mal_id}`, {
-				state: { animeData: animeObject },
-			});
-		} else if (typeof animeObject === 'number') {
-			navigate(`/anime/${animeObject}`);
-		}
-	};
-	const slides = [
-		{
-			src: 'https://cdn.myanimelist.net/images/anime/1659/154920l.jpg',
-			title: 'Jujutsu Kaisen: Culling Game Arc',
-			description: 'Kolejny sezon cierpień młodego Yuuji Itadori!',
-			buttonAction: () => handleAnimeClick(57658),
-		},
-		{
-			src: 'https://cdn.myanimelist.net/images/anime/1508/152472l.jpg',
-			title: 'Enen no Shouboutai: San no Shou Part 2',
-			description:
-				'Ostatni sezon popularnej serii o strażakach walczących z nadprzyrodzonymi pożarami.',
-			buttonAction: () => handleAnimeClick(59229),
-		},
-		{
-			src: 'https://cdn.myanimelist.net/images/anime/1592/154049l.jpg',
-			title: 'Vigilante: Boku no Hero Academia Illegals 2nd Season',
-			description:
-				'Smutni po finale MHA? Osuszczie swoje łzy z drugim sezonem Vigilante!',
-			buttonAction: () => handleAnimeClick(61942),
-		},
-	];
-	return (
-		<div>
-			<main className='space-y-5 lg:flex lg:flex-col lg:items-center lg:justify-center'>
-				<ImageCarousel slides={slides} />
+      const [popularResponse, recentResponse, recomendedResponse] =
+        await Promise.all(requests);
+      setRecommendedAnime(recomendedResponse.data);
+      setPopularAnime(popularResponse.data);
+      setRecentAnime(recentResponse.data);
+    } catch (error) {
+      console.error("Error fetching popular anime:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchAllData();
+  }, []);
+  const handleAnimeClick = (animeObject) => {
+    if (animeObject && animeObject.mal_id) {
+      navigate(`/anime/${animeObject.mal_id}`, {
+        state: { animeData: animeObject },
+      });
+    } else if (typeof animeObject === "number") {
+      navigate(`/anime/${animeObject}`);
+    }
+  };
+  const slides = [
+    {
+      src: "https://cdn.myanimelist.net/images/anime/1768/156339l.jpg",
+      title: "Super no Ura de Yani Suu Futari",
+      description:
+        "Ahh wakacje, czujesz ten dym z grilla prawda? Czekaj... TO PAPIEROS!",
+      buttonAction: () => handleAnimeClick(62076),
+    },
+    {
+      src: "https://cdn.myanimelist.net/images/anime/1917/158371l.jpg",
+      title: "Youjo Senki II",
+      description: "Mała maszyna wojenna powraca w nowym sezonie!",
+      buttonAction: () => handleAnimeClick(49233),
+    },
+    {
+      src: "https://cdn.myanimelist.net/images/anime/1615/158194l.jpg",
+      title: "Grand Blue Season 3",
+      description: "Szklaneczka wody? Pewnie! Dziękuję ci... O boże, o kur**",
+      buttonAction: () => handleAnimeClick(62542),
+    },
+  ];
+  return (
+    <div>
+      <main className="space-y-5 lg:flex lg:flex-col lg:items-center lg:justify-center">
+        <ImageCarousel slides={slides} />
 
-				{loading ? (
-					// 2. Blok ładowania - renderuje "szkieletowe" wersje AnimeCardSection
-					<>
-						{/* Szkielet dla "Popularne teraz" */}
-						<section className='p-3 lg:max-w-2/3 w-full'>
-							<div className='h-7 w-48 bg-gray-700 rounded-md animate-pulse mb-3'></div>
-							<Swiper
-								modules={[Navigation]}
-								navigation
-								spaceBetween={15}
-								slidesPerView={'auto'}
-							>
-								{Array.from({ length: 6 }).map((_, i) => (
-									<SwiperSlide
-										key={i}
-										className='!w-[185px] lg:!w-[250px] mt-5'
-									>
-										<AnimeCardSkeleton />
-									</SwiperSlide>
-								))}
-							</Swiper>
-						</section>
+        {loading ? (
+          // 2. Blok ładowania - renderuje "szkieletowe" wersje AnimeCardSection
+          <>
+            {/* Szkielet dla "Popularne teraz" */}
+            <section className="p-3 lg:max-w-2/3 w-full">
+              <div className="h-7 w-48 bg-gray-700 rounded-md animate-pulse mb-3"></div>
+              <Swiper
+                modules={[Navigation]}
+                navigation
+                spaceBetween={15}
+                slidesPerView={"auto"}
+              >
+                {Array.from({ length: 6 }).map((_, i) => (
+                  <SwiperSlide
+                    key={i}
+                    className="!w-[185px] lg:!w-[250px] mt-5"
+                  >
+                    <AnimeCardSkeleton />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </section>
 
-						{/* Szkielet dla "Ostatnio dodane" */}
-						<section className='p-3 lg:max-w-2/3 w-full'>
-							<div className='h-7 w-48 bg-gray-700 rounded-md animate-pulse mb-3'></div>
-							<Swiper
-								modules={[Navigation]}
-								navigation
-								spaceBetween={15}
-								slidesPerView={'auto'}
-							>
-								{Array.from({ length: 6 }).map((_, i) => (
-									<SwiperSlide
-										key={i}
-										className='!w-[185px] lg:!w-[250px] mt-5'
-									>
-										<AnimeCardSkeleton />
-									</SwiperSlide>
-								))}
-							</Swiper>
-						</section>
+            {/* Szkielet dla "Ostatnio dodane" */}
+            <section className="p-3 lg:max-w-2/3 w-full">
+              <div className="h-7 w-48 bg-gray-700 rounded-md animate-pulse mb-3"></div>
+              <Swiper
+                modules={[Navigation]}
+                navigation
+                spaceBetween={15}
+                slidesPerView={"auto"}
+              >
+                {Array.from({ length: 6 }).map((_, i) => (
+                  <SwiperSlide
+                    key={i}
+                    className="!w-[185px] lg:!w-[250px] mt-5"
+                  >
+                    <AnimeCardSkeleton />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </section>
 
-						{/* Szkielet dla "Dla początkujących" */}
-						<section className='p-3 lg:max-w-2/3 w-full'>
-							<div className='h-7 w-48 bg-gray-700 rounded-md animate-pulse mb-3'></div>
-							<Swiper
-								modules={[Navigation]}
-								navigation
-								spaceBetween={15}
-								slidesPerView={'auto'}
-							>
-								{Array.from({ length: 6 }).map((_, i) => (
-									<SwiperSlide
-										key={i}
-										className='!w-[185px] lg:!w-[250px] mt-5'
-									>
-										<AnimeCardSkeleton />
-									</SwiperSlide>
-								))}
-							</Swiper>
-						</section>
-					</>
-				) : (
-					// 3. Blok z prawdziwymi danymi (bez zmian)
-					<>
-						<AnimeCardSection
-							h2='Popularne teraz'
-							animeList={popularAnime}
-							onAnimeClick={handleAnimeClick}
-						/>
-						<AnimeCardSection
-							h2='Ostatnio dodane'
-							animeList={recentAnime}
-							onAnimeClick={handleAnimeClick}
-						/>
-						<AnimeCardSection
-							h2='Dla początkujących'
-							animeList={recomendedAnime}
-							onAnimeClick={handleAnimeClick}
-						/>
-					</>
-				)}
-			</main>
-		</div>
-	);
+            {/* Szkielet dla "Dla początkujących" */}
+            <section className="p-3 lg:max-w-2/3 w-full">
+              <div className="h-7 w-48 bg-gray-700 rounded-md animate-pulse mb-3"></div>
+              <Swiper
+                modules={[Navigation]}
+                navigation
+                spaceBetween={15}
+                slidesPerView={"auto"}
+              >
+                {Array.from({ length: 6 }).map((_, i) => (
+                  <SwiperSlide
+                    key={i}
+                    className="!w-[185px] lg:!w-[250px] mt-5"
+                  >
+                    <AnimeCardSkeleton />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </section>
+          </>
+        ) : (
+          // 3. Blok z prawdziwymi danymi (bez zmian)
+          <>
+            <AnimeCardSection
+              h2="Popularne teraz"
+              animeList={popularAnime}
+              onAnimeClick={handleAnimeClick}
+            />
+            <AnimeCardSection
+              h2="Ostatnio dodane"
+              animeList={recentAnime}
+              onAnimeClick={handleAnimeClick}
+            />
+            <AnimeCardSection
+              h2="Dla początkujących"
+              animeList={recomendedAnime}
+              onAnimeClick={handleAnimeClick}
+            />
+          </>
+        )}
+      </main>
+    </div>
+  );
 };
 
 export default Home;
